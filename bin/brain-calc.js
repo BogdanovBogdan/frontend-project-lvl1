732f36bfd20feb8e012edd7e7a35a2707e917d54
Bogdan Bogdanov <bogdanovbogdanv@gmail.com>
2023-05-11 08:45:17 +0700

    Add new game brain-calc.js

---
+++ bin/brain-calc.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import greeting, { getName } from '../src/cli.js';
+
+function getOperand() {
+  const randomNumber = Math.random() * 100;
+  return Math.round(randomNumber);
+}
+
+function getRandomOperator() {
+  const availableOperators = ['+', '-', '*'];
+  const randomIdx = Math.floor(Math.random() * availableOperators.length);
+  return availableOperators[randomIdx];
+}
+
+function calculateExpression(firstOperand, secondOperand, operator) {
+  switch (operator) {
+    case '+':
+      return firstOperand + secondOperand;
+    case '-':
+      return firstOperand - secondOperand;
+    case '*':
+      return firstOperand * secondOperand;
+    default:
+      throw new Error(`No logic found for operator ${operator}`);
+  }
+}
+
+function calcGame() {
+  const name = getName();
+  const numberAttemps = 3;
+  let correctAnswers = 0;
+
+  for (let i = 0; i < numberAttemps; i += 1) {
+    const firstOperand = getOperand();
+    const secondOperand = getOperand();
+    const mathOperator = getRandomOperator();
+    const result = calculateExpression(
+      firstOperand,
+      secondOperand,
+      mathOperator
+    );
+    console.log(`Question: ${firstOperand} ${mathOperator} ${secondOperand}`);
+    const answer = Number(readlineSync.question('Your answer: '));
+    const isCorrectAnswer = answer === result;
+
+    if (isCorrectAnswer) {
+      console.log('Correct!');
+      correctAnswers += 1;
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${result}`);
+      break;
+    }
+  }
+
+  if (correctAnswers === numberAttemps) {
+    console.log(`Congratulations, ${name}!`);
+  } else {
+    console.log(`Let's try again, ${name}!`);
+  }
+}
+
+function init() {
+  greeting();
+  console.log('What is the result of the expression?');
+  calcGame();
+}
+
+init();
