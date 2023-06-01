bae37acf9ce41b482f6bdcee68b35a9a1ea0efe7
Bogdan Bogdanov <bogdanovbogdanv@gmail.com>
2023-06-01 10:26:10 +0700

    Remove unnecessary comment block

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -44,11 +44,3 @@ function primeGame({ numberAttemps = 3, userName }) {
 }
 
 gameWrapper(primeGame);
-
-/** todo: почитать:
- * 1) https://skysmart.ru/articles/mathematic/chto-takoe-kvadratnyj-koren
- * 2) https://www.youtube.com/watch?v=s8zUK5jmXA4
- * 3) https://qna.habr.com/q/587696
- * 4) https://ru.wikipedia.org/wiki/%D0%9F%D1%80%D0%B5%D0%B4%D0%B8%D0%BA%D0%B0%D1%82
- * 5) https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%BF%D1%80%D0%BE%D1%81%D1%82%D1%8B%D1%85_%D1%87%D0%B8%D1%81%D0%B5%D0%BB#%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B5_500_%D0%BF%D1%80%D0%BE%D1%81%D1%82%D1%8B%D1%85_%D1%87%D0%B8%D1%81%D0%B5%D0%BB
- */
